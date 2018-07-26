bc39b090a4eaab69ea56fe4abb7c36ea692c950b
lucienshui <18952904@qq.com>
2018-07-26 20:41:20 +0800

    fix something

--- handle.php
+++ handle.php
@@ -12,14 +12,14 @@ if (isset($_POST['id'])) {
 $request_url = $_SERVER["REQUEST_URI"]; // 取当前路由的后缀
 if (preg_match('/^\/p\/[0-9]*$/', $request_url) == 0) {
     echo "<script> alert('请确认索引是否存在') </script>";
-//    header("Refresh:0;url=/" . $url);
+    header("Refresh:0;url=/" . $url);
 } else {
     require 'util/tableEditor.php';
     $it = new tableEditor();
     $id = str_replace('/p/', '', $request_url);
     if (!$it->exists($id)) {
         echo "<script> alert('请确认索引是否存在') </script>";
-//        header("Refresh:0;url=/" . $url);
+        header("Refresh:0;url=/" . $url);
     } else {
         $password = $it->password($id);
         $flag = True;
