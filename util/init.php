77ded6261ef7c28ffcc2a6656b9c58c0de864468
lucienshui <18952904@qq.com>
2018-08-12 10:16:49 +0800

    fix init.php

--- util/init.php
+++ util/init.php
@@ -1,15 +1,17 @@
 <?php
-require_once('dbEditor.php');
-$db = new dbEditor();
+$config = require_once('config.php');
+$connection = mysqli_connect($config['dbhost'], $config['username'], $config['password']);
+if (!$connection) die('Error' . mysqli_error($connection));
 $sqlSet = array(
-    'CREATE TABLE IF NOT EXISTS `id` (`id` int PRIMARY KEY)',
-    'INSERT INTO `id` VALUES (100)',
-    'CREATE TABLE IF NOT EXISTS `paste` (
+    "CREATE TABLE IF NOT EXISTS `id` (`id` int PRIMARY KEY)",
+    "INSERT INTO `id` VALUES (100)",
+    "CREATE TABLE IF NOT EXISTS `paste` (
       `id` int PRIMARY KEY,
 	  `text` text,
 	  `type` varchar(107),
-	  `password` varchar(37))'
+	  `password` varchar(37))"
 );
-foreach ($sqlSet as $sql) if (!$db->query($sql)) die('Error');
+foreach ($sqlSet as $sql) if (!$connection->query($sql)) die('Error');
 echo 'Success';
+unlink('init.php')
 ?>
